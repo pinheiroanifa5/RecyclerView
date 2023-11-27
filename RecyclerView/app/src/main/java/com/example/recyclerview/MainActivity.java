1f7541ebc6b0d898ecc2fd9abf7804b7e28e98d1
Anifa Pinheiro <63198620+pinheiroanifa5@users.noreply.github.com>
2023-11-27 18:03:59 -0100

    Add files via upload

---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.recyclerview;
+
+import static com.example.recyclerview.R.layout.activity_main;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    List<ListElement> elements;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_main);
+
+        init();
+    }
+
+    public void init() {
+        elements = new ArrayList<>();
+        elements.add(new ListElement("#775447", "Scuty", "Inativo"));
+        elements.add(new ListElement("#607d8b", "Any", "Activo"));
+        elements.add(new ListElement("#03a9f4", "Catelene", "Activo"));
+        elements.add(new ListElement("#f44336", "Kevin", "Activo"));
+        elements.add(new ListElement("#009688", "Victor", "Activo"));
+
+        ListAdapter listAdapter = new ListAdapter(elements, this);
+        RecyclerView recyclerView = findViewById(R.id.listRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(listAdapter); // Definir o adaptar no RecyclerView
+    }
+}
